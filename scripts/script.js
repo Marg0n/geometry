bcfcf9c343129eefc3109c44f784ffa95eaff573
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-02-15 15:38:41 +0600

    js intregation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log('loaded')
+function calculateArea() {
+  //base and lenght
+}
+
+function getInputById(element) {
+  const inputFieldText = document.getElementById(element).value;
+  const innputValue = parseFloat(inputFieldText);
+  return innputValue;
+}
