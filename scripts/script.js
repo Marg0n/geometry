dca36b53d4724a8778027924d2d4cddeefe9e88f
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-02-16 21:57:59 +0600

    get set function for triangle

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,23 @@
 function calculateArea() {
   //base and lenght
+  const a = getInputById("triangleBase");
+  const b = getInputById("triangleHeight");
+
+  // calculating
+  const area = a * b;
+
+  setInnertextById("result", area);
+
+  console.log(a, b, area);
 }
 
 function getInputById(element) {
   const inputFieldText = document.getElementById(element).value;
-  const innputValue = parseFloat(inputFieldText);
-  return innputValue;
+  const inputValue = parseFloat(inputFieldText);
+  return inputValue;
+}
+
+function setInnertextById(elementId, area) {
+  const setElement = document.getElementById(elementId);
+  setElement.value = area;
 }
